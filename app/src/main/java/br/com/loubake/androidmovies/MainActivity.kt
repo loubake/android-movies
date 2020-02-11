d2d6d89fd24e46c2512dd9afff080b77eaecd72a
loubake <loubake@hotmail.com>
2020-02-11 10:56:26 -0300

    Initial project files.

---
+++ app/src/main/java/br/com/loubake/androidmovies/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.loubake.androidmovies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
