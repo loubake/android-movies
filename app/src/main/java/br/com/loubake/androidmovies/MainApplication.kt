eb11cdda3a20741bc03ebc5fdb1b9f7c59534d29
Loubake <loubake@hotmail.com>
2023-09-28 13:23:40 +0200

    Update project and Android libraries. Migrating build.gradle to Kotlin DSL.

--- app/src/main/java/br/com/loubake/androidmovies/MainApplication.kt
+++ app/src/main/java/br/com/loubake/androidmovies/MainApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import br.com.loubake.androidmovies.di.moviesModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
+import org.koin.core.context.GlobalContext.startKoin
 
 class MainApplication : Application() {
 
